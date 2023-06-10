81d68772c72cb47b0b17736be961c4c9f0fd5c3e
valeria-barsukova <barsukova_valeria@mail.ru>
2023-06-10 13:15:21 +0300

    5.1 wsgi

--- wsgi_demo.py
+++ wsgi_demo.py
@@ -1,5 +1,15 @@
 from wsgiref.simple_server import make_server
 
+
+class Reverseware:
+    def __init__(self, app):
+        self.wrapped_app = app
+
+    def __call__(self, environ, start_response, *args, **kwargs):
+        wrapped_app_response = self.wrapped_app(environ, start_response)
+        return [data[::-1] for data in wrapped_app_response]
+
+
 def application(environ, start_response):
     response_body = [
         f'{key}: {value}' for key, value in sorted(environ.items())
