70794f1e5e9d83488c4677e45939bb25396f3c2d
valeria-barsukova <barsukova_valeria@mail.ru>
2023-06-10 19:23:35 +0300

    обработчики на основе классов

--- bumbo/api.py
+++ bumbo/api.py
@@ -1,3 +1,5 @@
+import inspect
+
 from parse import parse
 from webob import Request, Response
 
@@ -40,6 +42,11 @@ class API:
         handler, kwargs = self.find_handler(request_path=request.path)
 
         if handler is not None:
+            if inspect.isclass(handler):
+                handler = getattr(handler(), request.method.lower(), None)
+                if handler is None:
+                    raise AttributeError("Method not allowed", request.method)
+
             handler(request, response, **kwargs)
         else:
             self.default_response(response)
--- bumbo/app.py
+++ bumbo/app.py
@@ -4,11 +4,10 @@ from api import API
 app = API()
 
 
-@app.route("/home")
-def home(request, response):
-    response.text = "Hello from the HOME page"
+@app.route("/book")
+class BooksResource:
+    def get(self, req, resp):
+        resp.text = "Books Page"
 
-
-@app.route("/home")
-def home2(request, response):
-    response.text = "Hello from the SECOND HOME page"
+    def post(self, req, resp):
+        resp.text = "Endpoint to create a book"
