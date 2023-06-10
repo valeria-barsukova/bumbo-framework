234c1276a5771601a9b089c76ef00f1bc437b31c
valeria-barsukova <barsukova_valeria@mail.ru>
2023-06-10 21:24:11 +0300

    Пользовательские обработчики исключений

--- bumbo/app.py
+++ bumbo/app.py
@@ -15,8 +15,8 @@ class BooksResource:
     def delete(self, req, resp):
         resp.text = "Delete Book"
 
-    def put(self, req, resp):
-        resp.text = "Endpoint to update a book"
+    # def put(self, req, resp):
+    #     resp.text = "Endpoint to update a book"
 
 def handler(req, resp):
     resp.text = "sample"
@@ -29,4 +29,13 @@ def template_handler(req, resp):
         "index.html",
         context={"name": "Bumbo", "title": "Best Framework"}
     ).encode()
+
+def custom_exception_handler(request, response, exception_cls):
+    response.text = str(exception_cls)
+
+app.add_exception_handler(custom_exception_handler)
+
+@app.route("/exception")
+def exception_throwing_handler(request, response):
+    raise AssertionError("This handler should not be used.")
 
